43415412acf5c4108ac5ce3ade6c57139a6f6c10
abdulraheem381 <shadowpulse.pentest@gmail.com>
2025-06-28 21:25:24 +0500

    day_01 Project code updated

--- Day_01/Project_01_band_name_generator.py
+++ Day_01/Project_01_band_name_generator.py
@@ -3,6 +3,6 @@ print("Welcome to the Band Name Generator.")
 
 city_name = input("What's the name of the city you grew up in?\n")
 
-pet_name = input("What's your's prt name?\n")
+pet_name = input("What's your's pet name?\n")
 
 print("Your bamd name could be " + city_name + " " +  pet_name)
