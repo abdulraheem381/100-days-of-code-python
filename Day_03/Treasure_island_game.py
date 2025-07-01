b0d7890800d01ac8ed04c50afea40d71aef6e3de
abdulraheem381 <shadowpulse.pentest@gmail.com>
2025-07-01 05:40:33 +0500

    treasure file modified

--- Day_03/Treasure_island_game.py
+++ Day_03/Treasure_island_game.py
@@ -1,6 +1,7 @@
 import time
-print('''
+import sys
 
+print(r'''
 *******************************************************************************
           |                   |                  |                     |
  _________|________________.=""_;=.______________|_____________________|_______
@@ -15,20 +16,51 @@ print('''
 |                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
 |___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
 ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_ 
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____ 
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_ 
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____ 
 /______/______/______/______/______/______/______/______/______/______/_______/
 *******************************************************************************
 ''')
 
-print("Welcome to the Treasure island!")
+print("🏴‍☠️ Welcome to *Treasure Island*! 🏝️")
 time.sleep(1)
-print("Your mission is to find the Treasure.")
+print("Your mission is simple: **Find the hidden treasure.** 💰")
+time.sleep(1)
 
-road = input(("you're at a cross road. where do you want to go?\n\tType 'left' or 'right'\n ").upper)
+# Round 1
+round_1 = input("\nYou're at a crossroad. Where do you want to go?\n👉 Type 'left' or 'right': ").lower()
 
-if road == 'left': 
-    print("")
+if round_1 == 'right':
+    print("💥 You fell into a hole. Game Over!")
+    sys.exit()
+elif round_1 == 'left':
+    print("🌊 You arrive at a calm lake. There's an island in the middle.")
+else:
+    print("❌ Invalid choice. Try again next time.")
+    sys.exit()
 
+# Round 2
+round_2 = input("\nDo you want to wait for a boat or swim across?\n👉 Type 'wait' or 'swim': ").lower()
+
+if round_2 == 'swim':
+    print("🐟 You were attacked by a school of trout. Game Over!")
+    sys.exit()
+elif round_2 == 'wait':
+    print("⛵ You safely reach the island by boat.")
+else:
+    print("❌ Invalid choice. Try again next time.")
+    sys.exit()
+
+# Round 3
+round_3 = input("\nYou see a house with 3 doors: 🔴 Red, 🟡 Yellow, 🔵 Blue.\nWhich door do you choose?\n👉 Type 'red', 'yellow' or 'blue': ").lower()
+
+if round_3 == 'red':
+    print("🔥 Burned by fire. Game Over!")
+elif round_3 == 'blue':
+    print("🧟 Eaten by beasts. Game Over!")
+elif round_3 == 'yellow':
+    print("🎉 Congratulations! You found the treasure. **YOU WIN!** 🏆")
+else:
+    print("❌ That door doesn't exist. Game Over.")
