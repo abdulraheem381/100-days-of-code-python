31dfb83c5be163141072c42befccb1fdf6908b1f
abdulraheem381 <shadowpulse.pentest@gmail.com>
2025-07-24 12:29:12 +0500

    blackjack updated

--- Day_11/The_blackjack_capstone.py
+++ Day_11/The_blackjack_capstone.py
@@ -21,56 +21,68 @@ banner = r"""
            BLACK JACK CAPSTONE
 """
 
+while True:
+    game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
+
+    if game == 'n':
+        print("See you next time!")
+        exit()
+    else:
+        print(banner)
+
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+    cards_owned_by_user = []
+    cards_owned_by_computer = []
+    current_user_score = 0
+    current_comp_score = 0
+
+    def user_2_cards_choice():
+        user_cards = random.sample(cards, 2)
+        cards_owned_by_user.extend(user_cards)
+        return user_cards
+
+    def user_1_card_choice():
+        new_card = random.choice(cards)
+        cards_owned_by_user.append(new_card)
+        return new_card
+
+    def comp_1_card():
+        new_card = random.choice(cards)
+        cards_owned_by_computer.append(new_card)
+        return new_card
+
+    def comp_hidden_choice():
+        hidden_card = random.choice(cards)
+        cards_owned_by_computer.append(hidden_card)
+        return hidden_card
+
+    def add_user_cards():
+        return sum(cards_owned_by_user)
+
+    def add_comp_cards():
+        return sum(cards_owned_by_computer)
+    
+    user_2_cards_choice()
+    current_user_score = add_user_cards()
+
+    print(f"\tYour cards: {cards_owned_by_user}, current score: {current_user_score}")
+
+    comp_1_card()
+
+    print(f"\tComputer's first card: {cards_owned_by_computer}")
+
+   
 
 
-game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
 
-if game == 'n':
-    print("See you next time!")
-    exit()
-else:
-    print(banner)    
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-# User Cards Choice 
 
-cards_owned = []
-cards_owned_by_computer = []
 
-your_cards = random.sample(cards, 2)
-cards_owned.extend(your_cards)
 
     
-current_score = sum(your_cards)
 
 
-print(f"\tYour cards: {your_cards}, current score: {current_score}")
-
-# Computer Choice 
 
 
-computer_cards = random.choice(cards)
-cards_owned_by_computer.append(computer_cards)
-
-print(f"\tComputer's first card: {computer_cards}")
-
-
-# let's begin
-
-choice = input("Type 'y' to get another card, type 'n' to pass: ").lower()
-
-def add_card():
-    new_card = random.choice(cards)
-    cards_owned.append(new_card)
-
-
-if choice == 'y':
-    add_card()    
-else:
-    print(f"\tYour Final hand: {cards_owned}, final score: {sum(cards_owned)}")
-    exit()
-
-print(f"\tYour cards: {cards_owned}, current score: {sum(cards_owned)}")
-
-print(f"\tComputer's first card: {computer_cards}")
